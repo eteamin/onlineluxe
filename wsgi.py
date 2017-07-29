882a7bf59295e270df0ea137c0dad7a3b88d4132
Amin Etesamian <aminetesamian1371@gmail.com>
2017-07-29 04:00:34 +0200

    nt

---
+++ wsgi.py
@@ -0,0 +1,10 @@
+import os
+APP_CONFIG = "/home/onlinelux/onlinelux/production.ini"
+
+#Setup logging
+import logging.config
+logging.config.fileConfig(APP_CONFIG)
+
+#Load the application
+from paste.deploy import loadapp
+application = loadapp('config:%s' % APP_CONFIG)
